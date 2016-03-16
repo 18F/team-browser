ffc16ccf8f793a604c30f3e38d190be7e869c11c
Catherine Devlin <catherine.devlin@gsa.gov>
2016-03-16 16:13:24 -0400

    Catch common data errors before cryptic Jekyll errors result

--- _plugins/team.rb
+++ _plugins/team.rb
@@ -19,9 +19,22 @@ require 'team_hub/page'
 module Hub
   class Team
     def self.generate_pages(site)
+      self.check_data_quality(site)
       ::TeamHub::Page.generate_collection_item_pages(site, 'team',
         'team_member', 'full_name', primary_key: 'name')
       team = site.data['team'] || []
+    end
+
+    def self.check_data_quality(site)
+      # Liquid errors are very cryptic, so let's pre-check for
+      # some common data problems
+      %w(locations skills interests projects).each do |k|
+        raise "No data collected for #{k}" if site.data[k].nil?
+      end
+      raise "Bad location data" unless site.data['locations'].all? { |itm| itm.has_key? 'label' }
+      raise "Bad skills data" unless site.data['skills'].all? { |itm| itm.has_key? 'name' }
+      raise "Bad interests data" unless site.data['interests'].all? { |itm| itm.has_key? 'name' }
+      raise "Bad projects data" unless site.data['projects'].all? { |itm| itm.has_key? 'name' }
     end
   end
 end
