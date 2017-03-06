026ce105e1bda49cfafefb05652d4b7d1f045914
Eric Schles <ericschles@gmail.com>
2017-03-06 16:42:28 -0500

    adding routes

--- flask_template/app/commands.py
+++
@@ -1,8 +0,0 @@
-from flask_script import Command
-import code
-
-class REPL(Command):
-    "runs the shell"
-
-    def run(self):
-        code.interact(local=locals())
--- set_hmac_auth.sample.sh
+++
@@ -1,4 +0,0 @@
-# To access the private data on the team-api server,
-# get the HMAC shared secret and set it in the following
-# environment variable ("source" this file).
-export HMAC_KEY=""
